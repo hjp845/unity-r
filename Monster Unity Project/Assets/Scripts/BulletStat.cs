cb7673c41bd2767afb00b40495fa482c123a0fa2
Hwang Jung Pyeong <47222888+hjp845@users.noreply.github.com>
2020-02-24 02:43:09 +0900

    총알 관련 소스 코드 개선

---
+++ Monster Unity Project/Assets/Scripts/BulletStat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 변수의 저장기능만 하는 클래스는 VO value object
+public class BulletStat {
+
+    public float speed { get; set; }
+    public int damage { get; set; }
+
+    public BulletStat(float speed, int damage)
+    {
+        this.speed = speed;
+        this.damage = damage;
+    }
+}
